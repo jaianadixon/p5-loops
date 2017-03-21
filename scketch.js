26fd4c8db93c0d4c8fef5b181037e82b1c21b29d
Jaiana Dixon <jdixon@wlapcs.org>
2017-03-21 19:41:08 +0000

    first commit

---
+++ scketch.js
@@ -0,0 +1,25 @@
+function setup(){
+   createCanvas(600,400);
+   background(255,0,0);
+}
+
+function draw(){
+    strokeWeight(10);
+    stroke(255);
+    
+    var x=0;
+    fill(201, 0, 219)
+    while(x<width){
+      rect(x,   300, 25, 25);
+      x =x+ 50;  
+    }
+    
+    fill(226, 103, 2)
+    for(var x=0; x< width; x=x+ 50){
+       rect(x,   100, 25, 25); 
+    }
+    fill(0, 0, 0)
+    for(var x=0; x< width; x=x+ 50){
+       rect(x,   200, 25, 25); 
+    }
+}
